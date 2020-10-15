8f7a441e1e8e244c56a90dcef507341d5ab0df48
Ben Newman <bennewman5079@gmail.com>
2020-10-14 23:10:41 -0400

    Initial commit of ARGOS script

---
+++ ArgosTrackingTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ArgosTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Ben Newman (ben.newman@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
