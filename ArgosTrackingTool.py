eab1f605ad7adb11439ccd375d0d51ba14cd108b
Ben Newman <bennewman5079@gmail.com>
2020-10-15 03:16:52 -0400

    dictionaries added

--- ArgosTrackingTool.py
+++ ArgosTrackingTool.py
@@ -21,6 +21,10 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Create two empty dictionary object
+date_dict = {}
+coord_dict = {}
+
 #Iterate through all lines in the linelist
 for lineString in line_list:
     if lineString[0] == "#" or lineString[0] == 'u': continue
@@ -37,3 +41,6 @@ for lineString in line_list:
     
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon{obs_lon} on {obs_date}")
+    date_dict[record_id] = obs_date
+    coord_dict[record_id] = (obs_lat,obs_lon)
+    
